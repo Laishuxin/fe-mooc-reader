02ee3d40763b14fae4136104d9ed0a56e6f19e4f
Laishuxin <15622971239@163.com>
2022-10-15 14:29:21 +0800

    chore(front): 添加 vant 组件依赖

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,18 @@
-<script setup></script>
-
 <template>
   <div>
     app
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
-    
+
     <RouterView></RouterView>
+  </div>
+
+  <div>
+    button:
+    <br />
+    <van-button type="primary">
+      button
+    </van-button>
   </div>
 </template>
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,27 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import Components from "unplugin-vue-components/vite"
+import { VantResolver } from "unplugin-vue-components/resolvers"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
       "@stores": fileURLToPath(new URL("./src/stores", import.meta.url)),
       "@components": fileURLToPath(
-        new URL("./src/components", import.meta.url)
+        new URL("./src/components", import.meta.url),
       ),
     },
   },
-});
+})
